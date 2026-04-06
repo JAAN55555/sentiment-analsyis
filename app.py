757eda1bda630288ded6427ec179ef98099af07a
JAYASHREE JANANI MURUGESAN <152058383+JAAN55555@users.noreply.github.com>
2026-04-06 22:39:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,24 +1,29 @@
 from flask import Flask, render_template, request
 import os
-from transformers import pipeline
+import joblib
 
 app = Flask(__name__)
 
-# Load model once
-classifier = pipeline("sentiment-analysis")
+# Load your saved model
+model, vectorizer = joblib.load("sentiment_model.pkl")
 
 @app.route("/", methods=["GET", "POST"])
 def home():
     if request.method == "POST":
         text = request.form["text"]
 
-        result = classifier(text)[0]
-        label = result["label"]
-        confidence = round(result["score"] * 100, 2)
+        # Convert text to vector
+        text_vec = vectorizer.transform([text])
+
+        # Predict sentiment
+        prediction = model.predict(text_vec)[0]
+
+        # Simple confidence
+        confidence = 100
 
         return render_template(
             "index.html",
-            prediction=label,
+            prediction=prediction,
             confidence=confidence,
             text=text
         )
@@ -26,7 +31,7 @@ def home():
     return render_template("index.html")
 
 
-# IMPORTANT: Only ONE run block
+# IMPORTANT for Render
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
